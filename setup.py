804f6973ad66f5ebcb0c35a61c17ef04d6ac8ab4
PhilipPeters <35991743+PhilipPeters@users.noreply.github.com>
2021-01-04 17:09:33 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from distutils.core import setup
 setup(
   name = 'tweaked_graphviz',         # How you named your package folder (MyLib)
   packages = ['tweaked_graphviz'],   # Chose the same as "name"
-  version = '0.0.2',      # Start with a small number and increase it with every change you make
+  version = '0.0.3',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'A tweaked version of an interactive graphviz viewer for Dash',   # Give a short description about your library
   author = 'Philip Peters',                   # Type in your name
   author_email = 'pcpworld@yahoo.co.uk',      # Type in your E-Mail
   url = 'https://github.com/PhilipPeters/tweaked_graphviz',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/PhilipPeters/tweaked_graphviz/archive/0.0.2.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/PhilipPeters/tweaked_graphviz/archive/0.0.3.tar.gz',    # I explain this later on
   keywords = ['React', 'Dash', 'Graphviz'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'dash',
